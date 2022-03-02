997c90655765db03e72fabb51fd57f307064e336
JamesMolina <jjmolinaf12@gmail.com>
2022-03-01 20:55:37 -0600

    subiendo archivos

---
+++ src/tallergui/TallerGUI.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tallergui;
+
+/**
+ *
+ * @author James Josue Molina
+ */
+public class TallerGUI {
+
+    /*
+    MVC
+    Arboles
+    Ficheros con arboles
+    Guardar estado de arboles
+    Cargar estado de arboles
+    POPUPMENU con arboles
+    
+    Uso de jtables
+    
+    EXTRA
+    Graficas con libreria externa
+    Icono en ventanas
+    
+    */
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
